e91b554a83da34a0ef7a92f8e10f9180c695ffa9
Prithivi07 <prithivirajan2406@gmail.com>
2024-08-15 08:30:31 -0400

    Home page update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,7 @@
 const images = ["pic1.png","pic2.png","pic3.png"];
 let curr=0;
 function changeBackground() {
-    const bgdiv = document.querySelector(".bg");
+    const bgdiv = document.querySelector(".main");
     bgdiv.style.backgroundImage = `url(${images[curr]})`
     curr = (curr+1)% images.length;
 }
